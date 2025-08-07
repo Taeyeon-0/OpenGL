ab7e00bab52b6a95ed35e9d2577ab6c7cc0560de
Suciy-Mac <3278475092@qq.com>
2025-08-08 00:08:14 +0800

    update

--- src/2.lighting/3.materials/materials.cpp
+++ src/2.lighting/3.materials/materials.cpp
@@ -113,7 +113,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "basic_lighting_specular", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "basic_lighting_diffuse", nullptr, nullptr);
     if (window == nullptr) {
         PLOG_ERROR << "Failed to create GLFW window";
         glfwTerminate();
@@ -134,7 +134,7 @@ int main() {
     glEnable(GL_DEPTH_TEST);
 
     Shader lightingShader, lightCubeShader;
-    lightingShader.loadFromFiles("Shader/basic_light_specular.vs", "Shader/basic_light_specular.fs");
+    lightingShader.loadFromFiles("Shader/materials.vs", "Shader/materials.fs");
     lightCubeShader.loadFromFiles("Shader/light_cube.vs", "Shader/light_cube.fs");
 
     // first, configure the cube's VAO (and VBO)
@@ -175,11 +175,28 @@ int main() {
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        // be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
-        lightingShader.setVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
-        lightingShader.setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
-        lightingShader.setVec3("lightPos", lightPos);
+        lightingShader.setVec3("light.position", lightPos);
         lightingShader.setVec3("viewPos", camera.Position);
+
+        // light properties
+        glm::vec3 lightColor;
+        lightColor.x = static_cast<float>(sin(glfwGetTime() * 2.0));
+        lightColor.y = static_cast<float>(sin(glfwGetTime() * 0.7));
+        lightColor.z = static_cast<float>(sin(glfwGetTime() * 1.3));
+        glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);  // decrease the influence
+        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);// low influence
+        lightingShader.setVec3("light.ambient", ambientColor);
+        lightingShader.setVec3("light.diffuse", diffuseColor);
+        lightingShader.setVec3("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+
+        // material properties
+        lightingShader.setVec3("material.ambient", glm::vec3(1.0f, 0.5f, 0.31f));
+        lightingShader.setVec3("material.diffuse", glm::vec3(1.0f, 0.5f, 0.31f));
+        lightingShader.setVec3("material.specular", glm::vec3(0.5f, 0.5f, 0.5f));// specular lighting doesn't have full effect on this object's material
+        lightingShader.setFloat("material.shininess", 32.0f);
+
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
